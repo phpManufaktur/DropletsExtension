e510a77cbdbe843d2ab967e90ef2bd032fd8634c
hertsch <ralf.hertsch@phpmanufaktur.de>
2011-05-20 23:08:10 +0000

    git-svn-id: https://server3.subversion-server.com/phpmanufaktur/svn/dropletsExtension/trunk@3 4cb4cec7-5f3f-7748-9bca-814cc26f424e

---
+++ info.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * dropletExtension
+ * 
+ * @author Ralf Hertsch (ralf.hertsch@phpmanufaktur.de)
+ * @link http://phpmanufaktur.de
+ * @copyright 2011
+ * @license GNU GPL (http://www.gnu.org/licenses/gpl.html)
+ * @version $Id$
+ * 
+ * FOR VERSION- AND RELEASE NOTES PLEASE LOOK AT INFO.TXT!
+ */
+
+// prevent this file from being accessed directly
+if (!defined('WB_PATH')) die('invalid call of '.$_SERVER['SCRIPT_NAME']);
+
+$module_directory     = 'droplet_extension';
+$module_name          = 'dropletExtension';
+$module_function      = 'page';
+$module_version       = '0.10';
+$module_status        = 'Beta';
+$module_platform      = '2.8'; 
+$module_author        = 'Ralf Hertsch, Berlin (Germany)';
+$module_license       = 'GNU General Public License';
+$module_description   = 'dropletExtension - integrate droplets into the WebsiteBaker search function'; 
+$module_home          = 'http://phpmanufaktur.de/droplet_extension';
+$module_guid          = '2F1CCDD0-A922-4DDA-BDA8-FB0624F6C1FE';
+
+?>
---
+++ languages/DE.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * dropletExtension
+ * 
+ * @author Ralf Hertsch (ralf.hertsch@phpmanufaktur.de)
+ * @link http://phpmanufaktur.de
+ * @copyright 2011
+ * @license GNU GPL (http://www.gnu.org/licenses/gpl.html)
+ * @version $Id$
+ */
+
+// prevent this file from being accessed directly
+if (!defined('WB_PATH')) die('invalid call of '.$_SERVER['SCRIPT_NAME']);
+
+// Module Description
+$module_description 	= 'dropletExtension - Integriert Droplets in die WebsiteBaker Suchfunktion';
+// name of the person(s) who translated and edited this language file
+$module_translation_by = 'Ralf Hertsch (phpManufaktur)';
+
+
+?>
